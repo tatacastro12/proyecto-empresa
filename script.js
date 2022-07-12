fb7762e07d058d6ad60b5954b9c10c509fac978b
tatis <fundasercomoninos@gmail.com>
2022-07-12 13:50:21 -0500

    empresa

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const grande    = document.querySelector('.grande')
+const punto     = document.querySelectorAll('.punto')
+
+// Cuando CLICK en punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // QUITAR la clase activo de TODOS puntos
+    // AÑADIR la clase activo al punto que hemos hecho CLICK
+
+// Recorrer TODOS los punto
+punto.forEach( ( cadaPunto , i )=> {
+    // Asignamos un CLICK a cadaPunto
+    punto[i].addEventListener('click',()=>{
+
+        // Guardar la posición de ese PUNTO
+        let posicion  = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * -50
+
+        // MOVEMOS el grand
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos TODOS los punto
+        punto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+
+    })
+})
+
+
+let nombre= document.getElementById("nombres");
+let apellido= document.getElementById("apellidos");
+let correo= document.getElementById("correo");
+let contraseña= document.getElementById("pas");
+let registro= document.getElementById("reg");
+
+registro.addEventListener("click", () => {
+	console.log(nombre.textContent)
+    localStorage.setItem("nombre", nombre.value)
+    localStorage.setItem("apellido", apellido.value)
+    localStorage.setItem("correo", correo.value)
+    localStorage.setItem("mensaje", mensaje.value)
+
+})
+
+
+
+
+
+
+
